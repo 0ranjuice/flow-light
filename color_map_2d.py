3a8f61e20dfc84535718d934f359b2168e6ae4e3
0ranjuice <hank901122@gmail.com>
2023-03-20 19:05:10 +0800

    subtle adjustment

--- color_map_2d.py
+++ color_map_2d.py
@@ -20,7 +20,7 @@ def get_color_for_point(point_coords, list_of_point_centers, list_of_colors):
     # get distances of the "query" point from all other points
     distances = scipy.spatial.distance.cdist([point_coords],
                                              list_of_point_centers)[0]
-    
+
     # get weights and compute new RGB value as weighted sum:
     weights = 1 / (distances + 0.1)
     for ic, c in enumerate(list_of_colors):
@@ -28,10 +28,10 @@ def get_color_for_point(point_coords, list_of_point_centers, list_of_colors):
     color /= (np.sum(weights))
     sum_color = np.sum(color)
     required_sum_color = 600.0
-    if color.max() * (required_sum_color/sum_color) <= 255:
-        color *= (required_sum_color/sum_color)
+    if color.max() * (required_sum_color / sum_color) <= 255:
+        color *= (required_sum_color / sum_color)
     else:
-        color *= (255/(color.max()))
+        color *= (255 / (color.max()))
     return color
 
 
@@ -52,24 +52,24 @@ def create_2d_color_map(list_of_points, list_of_colors, height, width):
     c_x = int(width / 2)
     c_y = int(height / 2)
     step = 5
-    win_size = int((step-1) / 2)
+    win_size = int((step - 1) / 2)
+    # Standardization
     for i in range(len(list_of_points)):
         rgb[c_y - int(list_of_points[i][1] * height / 2),
             c_x + int(list_of_points[i][0] * width / 2)] = list_of_colors[i]
     for y in range(win_size, height - win_size, step):
         for x in range(win_size, width - win_size, step):
             x_real = (x - width / 2) / (width / 2)
-            y_real = (height / 2 - y ) / (height / 2)
+            y_real = (height / 2 - y) / (height / 2)
             color = get_color_for_point([x_real, y_real], list_of_points,
                                         list_of_colors)
-            rgb[y - win_size - 1 : y + win_size + 1,
-                x - win_size - 1 : x + win_size + 1] = color
+            rgb[y - win_size - 1: y + win_size + 1, x - win_size - 1: x + win_size + 1] = color
     bgr = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
     return bgr
 
 
 if __name__ == "__main__":
-    colors = {"coral": [255,127,80],
+    colors = {"coral": [255, 127, 80],
               "pink": [255, 192, 203],
               "orange": [255, 165, 0],
               "blue": [0, 0, 205],
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     sad_pos = [-0.6, -0.4]
     bgr = create_2d_color_map([angry_pos, fear_pos, happy_pos,
                                calm_pos, sad_pos],
-                              [colors["red"],  colors["yellow"],
+                              [colors["red"], colors["yellow"],
                                colors["orange"], colors["green"],
                                colors["blue"]], 200, 200)
     cv2.imshow('Signal', bgr)
